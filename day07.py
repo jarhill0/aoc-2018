90c4fbab756f93e4bee887a1e04a8c37fbfe9464
jarhill0 <jarhill0@users.noreply.github.com>
2018-12-07 09:34:12 -0500

    Day 7 part 2 -- weird algorithm that I don't like

--- day07.py
+++ day07.py
@@ -20,7 +20,7 @@ def part_a():
     for node in NODES:
         for connected in DAG[node]:
             degree[connected] += 1
-    zero_list = [node for node in NODES if degree[node] == 0]
+    zero_list = {node for node in NODES if degree[node] == 0}
 
     while len(zero_list) > 0:
         v = min(zero_list)
@@ -29,10 +29,48 @@ def part_a():
         for w in DAG[v]:
             degree[w] -= 1
             if degree[w] == 0:
-                zero_list.append(w)
+                zero_list.add(w)
 
     return ''.join(out)
 
 
+def task_time(l):
+    return 61 + (ord(l) - ord('A'))
+
+
+def part_b():
+    degree = defaultdict(int)
+    for node in NODES:
+        for connected in DAG[node]:
+            degree[connected] += 1
+    zero_list = {node for node in NODES if degree[node] == 0}
+
+    # there's GOTTA be a better algorithm than this stack of crap
+    work_needed = {node: task_time(node) for node in NODES}
+    current_tasks = set()
+    completed = set()
+    time_taken = 0
+    while len(completed) < len(NODES):
+        while len(current_tasks) < 5 and len(zero_list) > 0:
+            t = min(zero_list)
+            zero_list.remove(t)
+            current_tasks.add(t)
+
+        # we can assume that current_tasks is nonempty
+        work_step = min(work_needed[task] for task in current_tasks)
+        time_taken += work_step
+        for task in current_tasks:
+            work_needed[task] -= work_step
+            if work_needed[task] == 0:
+                completed.add(task)
+                for w in DAG[task]:
+                    degree[w] -= 1
+                    if degree[w] == 0:
+                        zero_list.add(w)
+        current_tasks.difference_update({task for task in current_tasks if work_needed[task] == 0})
+    return time_taken
+
+
 if __name__ == '__main__':
     print(part_a())
+    print(part_b())
