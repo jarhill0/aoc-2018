50d2f5542ef216f6e9cf624cb16ec72782ae7106
jarhill0 <jarhill0@users.noreply.github.com>
2018-12-09 00:46:46 -0500

    Better part B solution — linked list!

--- day09.py
+++ day09.py
@@ -40,8 +40,50 @@ def part_a():
     return max(score.values())
 
 
+class Marble:  # linked list thing... Cheap inserts, deletes
+    def __init__(self, num, left=None, right=None):
+        self.num = num
+        self.left = left
+        self.right = right
+
+        if self.left is not None:
+            self.left.right = self
+        if self.right is not None:
+            self.right.left = self
+
+    def remove(self):
+        assert self.right is not None
+        assert self.left is not None
+        self.right.left = self.left
+        self.left.right = self.right
+
+
 def part_b():
-    pass
+    current = Marble(0)
+    current.left = current
+    current.right = current
+
+    player = 0
+    score = defaultdict(int)
+    for marble in range(1, 100 * MARBLES + 1):
+        # print(circle)
+        if marble % 23 == 0:
+            # if marble % 1000 == 0:
+            #     print('{}/{}'.format(marble, MARBLES))
+
+            score[player] += marble
+            for _ in range(7):
+                current = current.left
+            score[player] += current.num
+            current = current.right
+            current.left.remove()
+        else:
+            current = current.right
+            current = Marble(marble, left=current, right=current.right)
+
+        player = (player + 1) % PLAYERS
+
+    return max(score.values())
 
 
 if __name__ == '__main__':
