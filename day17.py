065dcea9c8e686b6c6de4af489cb626ea254ac25
jarhill0 <git@reeshill.net>
2024-12-03 20:06:09 -0600

    Day 17 part 1
    I worked on this across multiple years. Started in December 2023 if not
    before. My initial approaches were weird and didn't quite work right.
    Eventually the insight I found was that I need the concept of a "row" —
    on an particular level, water can spread left or right before trying to
    flow down. If any of the row flows down without solidifying, the whole
    row flows without solidifying. Otherwise, the whole row solidifies. This
    captures the intuition that water spreads out left or right unless
    contained.

---
+++ day17.py
@@ -0,0 +1,121 @@
+from aoc_input import AOCInput
+
+CLAY = 1
+WATER = 2
+
+
+class Ground:
+    def __init__(self, grid):
+        self.grid = grid
+        self.water_reached = set()
+        self.overflowed = False
+        self.min_y = min(y for _, y in self.grid)
+        self.max_y = max(y for _, y in self.grid)
+        self.water_source = (500, 0)
+
+    def simulate(self):
+        try:
+            self.fill(self.water_source)
+        except RecursionError:
+            self.visualize()
+
+    def fill(self, pt):
+        if pt[1] > self.max_y or pt in self.grid:
+            return
+        if pt[1] >= self.min_y:
+            self.water_reached.add(pt)
+
+        below = (pt[0], pt[1] + 1)
+        if (
+            below not in self.grid
+            and below[1] <= self.max_y
+            and below not in self.water_reached
+        ):
+            self.fill(below)
+
+        if below not in self.grid:
+            return
+
+        row = [pt]
+        flowed_out = False
+        left = (pt[0] - 1, pt[1])
+        while left not in self.grid:
+            row.append(left)
+            self.water_reached.add(left)
+            below = (left[0], left[1] + 1)
+            if below not in self.grid:
+                self.fill(below)
+                if below not in self.grid:
+                    flowed_out = True
+                    break
+            left = (left[0] - 1, left[1])
+        right = (pt[0] + 1, pt[1])
+        while right not in self.grid:
+            row.append(right)
+            self.water_reached.add(right)
+            below = (right[0], right[1] + 1)
+            if below not in self.grid:
+                self.fill(below)
+                if below not in self.grid:
+                    flowed_out = True
+                    break
+            right = (right[0] + 1, right[1])
+
+        if not flowed_out:
+            for p in row:
+                self.grid[p] = WATER
+
+    def visualize(self):
+        min_x = min(min(x for x, _ in self.grid), min(x for x, _ in self.water_reached))
+        max_x = max(max(x for x, _ in self.grid), max(x for x, _ in self.water_reached))
+        min_y = min(y for _, y in self.grid)
+        max_y = max(y for _, y in self.grid)
+        for y in range(min_y, max_y + 1):
+            for x in range(min_x, max_x + 1):
+                square = self.grid.get((x, y))
+                char = {None: " ", CLAY: "#", WATER: "~"}[square]
+                if (x, y) in self.water_reached:
+                    if square == CLAY:
+                        char = "🙅"
+                    elif square is None:
+                        char = "|"
+                print(char, end="")
+            print("")
+
+
+def parse(inp):
+    grid = {}
+    for line in inp.value.splitlines():
+        single, _, rnge = line.partition(", ")
+        mx = {"x": 0, "y": 1}
+        pt = [0, 0]
+
+        n, _, v = single.partition("=")
+        pt[mx[n]] = int(v)
+
+        n, _, r = rnge.partition("=")
+        a, _, b = r.partition("..")
+        for v in range(int(a), int(b) + 1):
+            pt[mx[n]] = v
+            grid[tuple(pt)] = CLAY
+    return Ground(grid)
+
+
+def part_a(inp):
+    g = parse(inp)
+    g.simulate()
+    return len(g.water_reached)
+
+
+def part_b(inp):
+    return None
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.setrecursionlimit(2000)
+
+    INP = AOCInput(17)
+    print(part_a(INP))
+    print(part_b(INP))
---
+++ tests/test_day17.py
@@ -0,0 +1,32 @@
+import unittest
+
+from aoc_input import AOCInput
+import day17
+
+
+INPUT = AOCInput(17)
+EXAMPLE = AOCInput.test_input(
+    """x=495, y=2..7
+y=7, x=495..501
+x=501, y=3..7
+x=498, y=2..4
+x=506, y=1..2
+x=498, y=10..13
+x=504, y=10..13
+y=13, x=498..504"""
+)
+
+
+class TestDay17(unittest.TestCase):
+    def test_part_a(self):
+        self.assertEqual(57, day17.part_a(EXAMPLE))
+        actual_answer = day17.part_a(INPUT)
+        # self.assertEqual(None, actual_answer)
+
+    def test_part_b(self):
+        self.assertEqual(None, day17.part_b(EXAMPLE))
+        # self.assertEqual(None, day17.part_b(INPUT))
+
+
+if __name__ == "__main__":
+    unittest.main()
